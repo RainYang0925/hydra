f462c18fef644ac229e8b6caaf494899d878e44c
任志超 <zren@xiaohongshu.com>
2017-04-18 19:52:22 +0800

    clean the contain specific for Xiaohongshu

--- js/test.js
+++ js/test.js
@@ -1,28 +1,9 @@
 describe("This is the suite: Just test xiaohongshu can open", function() {
-  var a;
-
-  it("This is a case: Let's open the chrome and navigate to www.xiaohongshu.com ", function() {
-    a = true;
-    expect(a).toBe(true);
-    importPackage('com.xiaohongshu.automation.service.webdriver');
-    importPackage('com.xiaohongshu.automation.service.webdriver.common');
-    //importClass('com.xiaohongshu.automation.service.webdriver.common.PageObject');
-    var p = new DefaultPageObject('http://www.xiaohongshu.com',null);
-    try {
-        //p.load();
-        expect(p.get)
-        caseResult.setResultMsg('page loaded!')
-    }catch(err){
-        err.printStackTrace();
-    }
-    //caseResult.setResultMsg('succeed!');
-  });
-
-
+  var appKey = "fc8d52972b64e3bb" ;
   it("This is another case: test simple rest api", function(){
     var restfulUtils = new RestfulUtils();
-    restfulUtils.setServerURL("http://localhost:3000/posts");
-    //restfulUtils.setBinaryFile("/Users/zhanghao/Documents/Work/test/test.zip");
+    var url = "http://api.jisuapi.com/iqa/query?appkey=" + appKey + "&question=上海人口" ;
+    restfulUtils.setServerURL(url);
     restfulUtils.setHttpMethod("get");
     restfulUtils.execute();
     JSLog.info(restfulUtils.getResponseBody());
